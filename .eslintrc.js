84da210aa126b8a4af73c0c8b58404b2ecd25cdf
Cnoccir <136872000+Cnoccir@users.noreply.github.com>
2024-07-09 23:24:09 -0400

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended"
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  rules: {
+    "no-unused-vars": "warn",
+    "no-console": "off",
+    "no-misleading-character-class": "warn",
+  },
+};
