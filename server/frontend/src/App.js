83f5da522ba2c7fb4fbfd43a35b1d493bb44c957
Cnoccir <Raymond@ame-inc.com>
2024-07-09 22:20:44 -0400

    Lint fixes for JS Round 1

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,8 +1,9 @@
-import LoginPanel from "./components/Login/Login"
+/* jshint esversion: 6 */
+import LoginPanel from "./components/Login/Login";
 import { Routes, Route } from "react-router-dom";
 import Register from "./components/Register/Register";
 import Dealers from './components/Dealers/Dealers';
-import Dealer from "./components/Dealers/Dealer"
+import Dealer from "./components/Dealers/Dealer";
 import PostReview from './components/Dealers/PostReview';
 
 function App() {
@@ -10,10 +11,11 @@ function App() {
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/dealers" element={<Dealers/>} />
-      <Route path="/dealer/:id" element={<Dealer/>} />
-      <Route path="/postreview/:id" element={<PostReview/>} />
+      <Route path="/dealers" element={<Dealers />} />
+      <Route path="/dealer/:id" element={<Dealer />} />
+      <Route path="/postreview/:id" element={<PostReview />} />
     </Routes>
   );
 }
+
 export default App;
